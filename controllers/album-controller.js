19036c2c15ddfec75bb094c5caab1e57052ea5b7
korenrolle <115126618+korenrolle@users.noreply.github.com>
2023-01-10 11:08:02 -0800

    Update album-controller.js

--- controllers/album-controller.js
+++ controllers/album-controller.js
@@ -1,26 +1,76 @@
-const Album = require("../models/album");
+const express = require('express')
+const router = express.Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
 
-exports.show = (req, res, next) => {
-    const id = req.params.id;
-    Album.findById(id)
-        .then(album => {
-            if (!album) {
-                const error = new Error("Album not found");
-                error.status = 404;
-                throw error;
-            }
-            res.json(album);
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+
+router.get('/', async (req, res, next) => {
+    try {
+        const allUser = await db.User.find()
+        return res.status(200).json(allUser)
+    } catch (err) {
+        console.error(err)
+        return next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const foundUser = await db.User.findById(req.params.id)
+        console.log(foundUser)
+        return res.status(200).json(foundUser)
+    } catch (err) {
+        console.error(err)
+        return next(err)
+    }
+})
+
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const passHash = await bcrypt.hash(req.body.password, salt)
+        req.body.password = passHash
+
+        const newUser = await db.User.create(req.body)
+        console.log(createdUser)
+        res.status(201).json({
+            user: newUser,
+            isLpggedIn: true
         })
-        .catch(error => next(error));
-};
+ 
+    } catch (err) {
+        console.error(err)
+        return next(err)
+    }
+})
 
-exports.create = (req, res, next) => {
-    const albumData = req.body;
-    const newAlbum = new Album(albumData);
-    newAlbum.save()
-        .then(album => {
-            res.status(201).json(album);
-        })
-        .catch(error => next(error));
-};
+router.post('/login', async (req, res, next) => {})
 
+router.put('/:id', async (req, res) => {
+    try {
+        
+
+        const updatedPerson = await db.User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(updatedPerson)
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deletedUser = await db.User.findByIdAndDelete(req.params.id)
+        console.log(deletedUser)
+        res.redirect('/')
+    } catch (err) {
+        console.error(err)
+        return next(err)
+    }
+
+})
+
+module.exports = router
