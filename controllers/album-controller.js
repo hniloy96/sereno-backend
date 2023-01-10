5a0355d8ec04e6d8cb82d0a2c5b52e9e40344891
korenrolle <115126618+korenrolle@users.noreply.github.com>
2023-01-10 10:57:23 -0800

    Update album-controller.js

--- controllers/album-controller.js
+++ controllers/album-controller.js
@@ -1,9 +1,25 @@
 const Album = require("../models/album");
 
-exports.index = (req, res, next) => {
-    Album.find()
-        .then(albums => {
-            res.json(albums);
+exports.show = (req, res, next) => {
+    const id = req.params.id;
+    Album.findById(id)
+        .then(album => {
+            if (!album) {
+                const error = new Error("Album not found");
+                error.status = 404;
+                throw error;
+            }
+            res.json(album);
+        })
+        .catch(error => next(error));
+};
+
+exports.create = (req, res, next) => {
+    const albumData = req.body;
+    const newAlbum = new Album(albumData);
+    newAlbum.save()
+        .then(album => {
+            res.status(201).json(album);
         })
         .catch(error => next(error));
 };
