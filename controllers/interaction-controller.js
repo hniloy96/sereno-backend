98ab7e651872d2a4c321174014681d52500b3133
hniloy96 <hamimniloy96@gmail.com>
2023-01-12 18:04:31 -0500

    changed it back to models

--- controllers/interaction-controller.js
+++ controllers/interaction-controller.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../test-models')
+const db = require('../models')
 const { handleValidateOwnership, requireToken } = require('../middleware/auth')
 
 
